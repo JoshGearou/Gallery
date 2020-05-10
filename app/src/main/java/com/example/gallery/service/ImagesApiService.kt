993e49bce49c944f8828f9e90c81a8cbd355e8bb
jgearou <josh.gearou@verizonmedia.com>
2020-05-10 11:53:16 -0500

    remove unecessary header

--- app/src/main/java/com/example/gallery/service/ImagesApiService.kt
+++ app/src/main/java/com/example/gallery/service/ImagesApiService.kt
@@ -3,14 +3,12 @@ package com.example.gallery.service
 import com.example.gallery.model.ImagesMetaData
 import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
-import retrofit2.http.Headers
 
 interface ImagesApiService {
 
     @GET("photos/")
     fun getImages(): Single<List<ImagesMetaData>>
 
-    @Headers
     @GET("photos/random/")
     fun getRandomImage(): Single<ImagesMetaData>
 }
